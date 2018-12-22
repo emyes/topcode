0ad6a66257b94e66c2b6911ff04427a5f1ecd0f8
SathishKumar <satishkumar.mohanraj@Standardbank.co.za>
2018-12-22 13:02:35 +0200

    Revert "Topic service refactored to make use of template and removed topic repository"
    This reverts commit 64d6c30cd45e7721d07feb33b4783a9bcebadeb6.

--- src/main/java/com/doppler/services/TopicService.java
+++ src/main/java/com/doppler/services/TopicService.java
@@ -3,8 +3,11 @@ package com.doppler.services;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.client.RestTemplate;
 
 import com.doppler.entities.Topic;
 import com.doppler.util.ApiConstants;
@@ -18,11 +21,17 @@ public class TopicService extends BaseService {
 
 	private static final String TOPIC_URL = "/topics";
 
+	private RestTemplate restTemplate = new RestTemplate();
+
 	@Autowired
-	BackendAPIService apiService;
+	LoginRestService loginService;
 
 	public List<Topic> retrieveTopics() {
-		List<Topic> topics = apiService.getForList(Topic.class, ApiConstants.BASE_URI + TOPIC_URL);
-		return topics;
+		// Logging into Application to obtain new session id
+		String token = loginService.login();
+		ResponseEntity<List> response = restTemplate.exchange(ApiConstants.BASE_URI + TOPIC_URL, HttpMethod.GET,
+				constructHttpEntityWithRequestHeaders(token), List.class);
+
+		return (List<Topic>) response.getBody();
 	}
 }
